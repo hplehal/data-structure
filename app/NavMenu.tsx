e56e9dc562f61aac72fc33916bd67109c1529bef
Tej Lehal <tejlehal@Tejs-MacBook-Pro.local>
2023-10-16 17:29:30 -0400

    Setup css snippets

---
+++ app/NavMenu.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function NavMenu() {
+    return(
+      <nav className="nav">
+        <h1 className="nav-header">Data Structure</h1>
+        <ul className="nav-menu">
+            <li>
+                <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link href={'/data-structure'}>Data Structure</Link>
+            </li>
+            <li>
+                <Link href={'/algorithm'}>Algorithms</Link>
+            </li>
+            <li>
+                <Link href={'/sample'}>Code Sample</Link>
+            </li>
+            <li>
+                <Link href={'/interview'}>Interview Questions</Link>
+            </li>
+        </ul>
+      </nav>  
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,6 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -82,12 +58,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
         </a>
       </div>
     </main>
